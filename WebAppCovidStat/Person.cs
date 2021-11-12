393969a49469aa62bc5933f79fa5f93a073db5e5
irinniada123 <irinniada123@gmail.com>
2021-11-12 16:57:53 +0200

    Add project files.

---
+++ WebAppCovidStat/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebAppCovidStat
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string City { get; set; }
+        public DateTime DayOfVaccination { get; set; }
+        public string Vaccine { get; set; }
+        public int VaccineDose { get; set; }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Vacced]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Birthday] DATE NOT NULL, 
+    [City] NVARCHAR(50) NOT NULL, 
+    [DayOfVaccination] DATE NOT NULL, 
+    [Vaccine] NVARCHAR(50) NOT NULL, 
+    [VaccineDose] INT NOT NULL
+)
